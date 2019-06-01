c881d969d2da2f4eadb1de6ccd5558876f2c530b
Aniruddha <aniruddha823@gmail.com>
2019-05-31 18:10:14 -0700

    fixed conversation timestamps in conversation view

--- JitJat/Views/ConversationCell.swift
+++ JitJat/Views/ConversationCell.swift
@@ -15,7 +15,7 @@ class ConversationCell: UITableViewCell {
         didSet {
             nameLabel.text = message?.senderName
             messageLabel.text = message?.text
-            timeLabel.text = setupTime(timestamp: (message?.timestamp)!)
+            timeLabel.text = getTimeString(timestamp: (message?.timestamp)!)
             profilePictureView.layer.cornerRadius = profilePictureView.bounds.height / 2
             profilePictureView.layer.borderWidth = 2
             profilePictureView.layer.borderColor = #colorLiteral(red: 0, green: 0.7843137255, blue: 0, alpha: 1)
@@ -29,10 +29,25 @@ class ConversationCell: UITableViewCell {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     
-    func setupTime(timestamp: Double) -> String {
-        let df = DateFormatter(); df.dateStyle = .none; df.timeStyle = .short;
-        let date = Date(timeIntervalSince1970: timestamp)
-        return df.string(from: date)
+    func getTimeString(timestamp: TimeInterval) -> String {
+        let fromDate = Date(timeIntervalSince1970: timestamp)
+        let currentDate = Date()
+        let formatter = DateFormatter()
+        let calendar = NSCalendar(calendarIdentifier: .gregorian); calendar?.locale = .current
+        var result = String()
+        
+        if let check = calendar?.isDate(fromDate, inSameDayAs: currentDate), check {
+            formatter.dateStyle = .none; formatter.timeStyle = .short
+            result = formatter.string(from: fromDate)
+        } else if let check = calendar?.isDateInYesterday(fromDate), check {
+            result = "Yesterday"
+        } else if ((calendar?.date(byAdding: .day, value: -7, to: currentDate))!...currentDate).contains(fromDate) {
+            result = (calendar?.weekdaySymbols[(calendar?.component(.weekday, from: fromDate))! - 1])!
+        } else {
+            formatter.dateStyle = .short; formatter.timeStyle = .none
+            result = formatter.string(from: fromDate)
+        }
+        
+        return result
     }
-    
 }
